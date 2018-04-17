8e309dea7766484652b36454e04097a59a3f6609
yunuusdemir <32935060+yunuusdemir@users.noreply.github.com>
2018-04-17 23:51:57 +0300

    Veritabanı .sql dosyası eklendi.

---
+++ mysql.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : tutorial
+ Source Server Type    : MySQL
+ Source Server Version : 50051
+ Source Host           : localhost:3306
+ Source Schema         : mysql
+
+ Target Server Type    : MySQL
+ Target Server Version : 50051
+ File Encoding         : 65001
+
+ Date: 17/04/2018 22:58:09
+*/
+
